613c7968043e2c50a676616b4879b08e88bc6390
Brad Gibbs <bradg@cs.utexas.edu>
2023-04-14 22:52:05 -0500

    edited run_qemu to include usb devices, created usb files for initialization

---
+++ kernel/usb.cc
@@ -0,0 +1,18 @@
+#include "usb.h"
+#include "stdint.h"
+#include "idt.h"
+#include "debug.h"
+#include "threads.h"
+#include "process.h"
+#include "machine.h"
+#include "ext2.h"
+#include "elf.h"
+#include "libk.h"
+#include "file.h"
+#include "heap.h"
+#include "shared.h"
+#include "kernel.h"
+
+void USB::init(void) {
+    // initialize usb ports
+}
---
+++ kernel/usb.h
@@ -0,0 +1,9 @@
+#ifndef _USB_H_
+#define _USB_H_
+
+class USB {
+public:
+    static void init(void);
+};
+
+#endif
