af225e88a6817ce8e6ead1c0f1cd55d3e6fe9b04
Brad Gibbs <bradg@cs.utexas.edu>
2023-04-18 14:11:21 -0500

    runqemu now has graphics enabled, made files for ps2

---
+++ kernel/ps2.cc
@@ -0,0 +1,23 @@
+#include "ps2.h"
+#include "stdint.h"
+#include "config.cc"
+#include "idt.h"
+#include "debug.h"
+#include "threads.h"
+#include "process.h"
+#include "machine.h"
+#include "ext2.h"
+#include "elf.h"
+#include "libk.h"
+#include "file.h"
+#include "heap.h"
+#include "shared.h"
+#include "kernel.h"
+
+void PS2::init(void) {
+    //determine if ps/2 controller exists
+    RSD* rsdp = findRSD();
+    uint32_t acpi_version = rsdp->Revision == 0 ? 1 : 2;
+    char* name;
+    SDT* rsdt = findSDT(rsdp, name);
+}
---
+++ kernel/ps2.h
@@ -0,0 +1,9 @@
+#ifndef _PS2_H_
+#define _UPS2_H_
+
+class PS2 {
+public:
+    static void init(void);
+};
+
+#endif
