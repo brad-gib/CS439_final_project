c9fa4b19eae87b422084d947cbb1eff804c0bd3a
brad-gib <57685364+brad-gib@users.noreply.github.com>
2023-04-18 23:59:09 -0500

    enabling mouse

--- kernel/marlon_ps2.h
+++ kernel/marlon_ps2.h
@@ -42,9 +42,9 @@ public:
 
 
 
-
-        // outb(0xD4, 0x64);                    // tell the controller to address the mouse
-        // outb(0xF3, 0x60);                    // write the mouse command code to the controller's data port
+        //enable mouse
+        outb(0x64, 0xD4);                    // tell the controller to address the mouse
+        outb(0x60, 0xF4);                    // write the mouse command code to the controller's data port
         // while(!(inb(0x64) & 1) asm("pause"); // wait until we can read
         // ack = inb(0x60);                     // read back acknowledge. This should be 0xFA
         // outb(0xD4, 0x64);                    // tell the controller to address the mouse
